f846915cb57fd7e739335bf062f4953404974321
JAgyei-Burobey <JAgyei-Burobey@qa.com>
2021-07-08 16:01:55 +0000

    completed challenge; testing the push

--- programs/example.py
+++ programs/example.py
@@ -12,7 +12,9 @@
     # <HINT>
 
     # What was the name of the function we have seen which changes the case of a string?  Use your CLI to access the Python documentation and get help(str).
-    
+
 def endsPy(input):
-    return True
+    # return input[-2:].lower() == 'py'
+    return input.casefold().endswith("py")
+    
     
